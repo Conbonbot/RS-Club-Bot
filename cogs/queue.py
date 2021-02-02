d0bbeba3020bd7fb5c33509d79a38bf3938b07e5
Connor Harvey <conbonbot745@gmail.com>
2021-02-02 14:36:55 -0800

    added queue functionallity

--- cogs/queue.py
+++ cogs/queue.py
@@ -1,5 +1,6 @@
 import os
 import random
+from discord import channel
 from dotenv import load_dotenv
 import sqlite3
 import datetime
@@ -17,55 +18,100 @@ class RSQueue(commands.Cog, name='Queue'):
         self.bot = bot
         self.index = 0
         self.printer.start()
-
-    def cog_unload(self):
-        self.printer.cancel()
-
-    @tasks.loop(seconds=5.0)
-    async def printer(self):
-        self.index += 1
-
-
-
-    @commands.command(aliases=["1"], help="Join a RS Queue with this command!")
-    async def one(self, ctx):
-        global rs_channel
-        rs_channel = {
-            "rs5-club" : "5", 
-            "rs6-club" : "6",
-            "rs7-club" : "7",
-            "rs8-club" : "8",
-            "rs9-club" : "9",
-            "rs10-club" : "10"
+        self.rs_channel = {
+            "rs5-club" : 5, 
+            "rs6-club" : 6,
+            "rs7-club" : 7,
+            "rs8-club" : 8,
+            "rs9-club" : 9,
+            "rs10-club" : 10,
+            "rs11-club" : 11
         }
-        global rs_ping
-        rs_ping = {
+        self.rs_ping = {
             "RS5" : "806018027383422998",
             "RS6" : "806018102797402122",
             "RS7" : "806018133835513876",
             "RS8" : "806018135106519040",
             "RS9" : "806018147487711313",
-            "RS10" : "806018150256082984"
+            "RS10" : "806018150256082984",
+            "RS11" : "806261078295183400"
         }
-        global rs_ping_1more
-        rs_ping_1more = {
+        self.rs_ping_1more = {
             "RS5" : "806018333517938688",
             "RS6" : "806018337804910592",
             "RS7" : "806018340203397140",
             "RS8" : "806018343696990208",
             "RS9" : "806018346269016084",
-            "RS10" : "806018349183139890"
+            "RS10" : "806018349183139890",
+            "RS11" : "806261118158372936"
         }
-        for club_channel in rs_channel:
-            if(str(ctx.message.channel) == club_channel):
-                await ctx.send("adding to queue")
+        self.emojis = {
+            "1️⃣" : 1,
+            "2️⃣" : 2,
+            "3️⃣" : 3,
+            "4️⃣" : 4
+        }
+
+    def cog_unload(self):
+        self.printer.cancel()
+
+    @tasks.loop(minutes=1.0)
+    async def printer(self):
+        self.index += 1
+
+    #@commands.command()
+    #async def clear(self, ctx, limit: int):
+    #    await ctx.channel.purge(limit=limit)
+
+
+    @commands.command(aliases=["1"], help="Join a RS Queue with this command!")
+    async def one(self, ctx):
+        right_channel = False
+        channel = ""
+        for club_channel in self.rs_channel:
+            if(club_channel == str(ctx.message.channel)):
+                right_channel = True
+                channel = club_channel
+        if(right_channel):
+            has_right_role = False
+            for role in ctx.author.roles:
+                if(str(role) == f'RS{self.rs_channel[channel]}'):
+                    has_right_role = True
+            if(has_right_role):
+                # This is where the fun begins
+                db = sqlite3.connect('rsqueue.sqlite')
+                cursor = db.cursor()
+                # check if they are in any other queues
+                sql = "SELECT user_id FROM main WHERE user_id=?" 
+                cursor.execute(sql, [(ctx.author.id)])
+                if(len(cursor.fetchall()) == 0): # They weren't found in the database, add them
+                    sql = "INSERT INTO main(user_id, amount, level) VALUES(?,?,?)"
+                    val = (ctx.author.id, 1, self.rs_channel[channel])
+                    cursor.execute(sql, val)
+                    # Print out the queue
+                    sql = "SELECT amount FROM main WHERE level=?"
+                    cursor.execute(sql, [(self.rs_channel[channel])])  
+                    total_people = cursor.fetchall()
+                    count = 0
+                    print(total_people)
+                    for people in total_people:
+                        count += int(people[0])
+                    print(count)
+                    db.commit()
+                    cursor.close()
+                    db.close()
             else:
-                await ctx.send("Command not run in an RS Channel")
-                break
+                await ctx.send(f"{ctx.author.mention}, you aren't RS{self.rs_channel[channel]}")
+        else:
+            msg = await ctx.send("Command not run in an RS Channel")
+            await asyncio.sleep(10)
+            await ctx.message.delete()
+            await msg.delete
+            
 
     @commands.command(aliases=["2"], help="Join a RS Queue with this command!")
     async def two(self, ctx):
-        for club_channel in rs_channel:
+        for club_channel in self.rs_channel:
             if(str(ctx.message.channel) == club_channel):
                 continue
             else:
@@ -74,12 +120,45 @@ class RSQueue(commands.Cog, name='Queue'):
 
     @commands.command(aliases=["3"], help="Join a RS Queue with this command!")
     async def three(self, ctx):
-        for club_channel in rs_channel:
+        for club_channel in self.rs_channel:
             if(str(ctx.message.channel) == club_channel):
                 continue
             else:
                 await ctx.send("Command not run in an RS Channel")
                 break
+
+    @commands.command(aliases=["q"])
+    async def queue(self, ctx):
+        queue_embed = discord.Embed(
+            color = discord.Color.red()
+        )
+        db = sqlite3.connect('rsqueue.sqlite')
+        cursor = db.cursor()
+        sql = "SELECT amount FROM main WHERE level=?"
+        cursor.execute(sql, [(self.rs_channel[str(ctx.message.channel)])])
+        people = cursor.fetchall()
+        count = 0
+        counting = []
+        for person in people:
+            counting.append(person[0])
+            count += int(person[0])
+        sql = "SELECT user_id FROM main WHERE level=?"
+        cursor.execute(sql, [(self.rs_channel[str(ctx.message.channel)])])
+        people_printing = cursor.fetchall()
+        list_people = []
+        for people in people_printing:
+            print((await self.bot.fetch_user(people[0])).display_name)
+            list_people.append((await self.bot.fetch_user(people[0])).display_name)
+        str_people = ""
+        emoji_count = 0
+        for i in range(len(people_printing)):
+            for j in range(counting[i]):
+                str_people += str(list(self.emojis)[emoji_count])
+                emoji_count += 1
+            str_people += " " + list_people[i]
+            str_people += "\n"
+        queue_embed.add_field(name=f"The Current RS{self.rs_channel[str(ctx.message.channel)]} Queue ({count}/4)", value=str_people, inline=False)
+        message = await ctx.send(embed=queue_embed)
 
 
 
--- cogs/role.py
+++ cogs/role.py
@@ -16,6 +16,15 @@ class RSRole(commands.Cog, name='Role'):
 
     def __init__(self, bot):
         self.bot = bot
+        self.emojis = {
+            '6️⃣' : 6,
+            '7️⃣' : 7,
+            '8️⃣' : 8,
+            '9️⃣' : 9,
+            '🔟' : 10,
+            '⏸️' : 11,
+            '❌' : -1,
+        }
 
     @commands.command(help="React to the message to join a rs level")
     async def role(self, ctx):
@@ -24,20 +33,35 @@ class RSRole(commands.Cog, name='Role'):
         )
         role_embed.add_field(name="React below to recieve the corresponding RS Role, and ❌ to remove all RS Roles", value="Current Levels: 6️⃣, 7️⃣, 8️⃣, 9️⃣, 🔟, ⏸️, ❌")
         message = await ctx.send(embed=role_embed)
-        emojis = ['6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟', '⏸️', '❌']
-        for emoji in emojis:
+        for emoji in self.emojis.keys():
             await message.add_reaction(emoji)
+        await ctx.message.delete()
+
 
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
+        # Example payload below
+        #<RawReactionActionEvent message_id=806247306047389696 user_id=805960284543385650 channel_id=805959424081920025 guild_id=805959424081920022 
+        # emoji=<PartialEmoji animated=False name='6️⃣' id=None> event_type='REACTION_ADD' 
+        # member=<Member id=805960284543385650 name='RS Club Bot' discriminator='3869' bot=True nick=None 
+        # guild=<Guild id=805959424081920022 name='RS Club Temp Server' shard_id=None chunked=False member_count=3>>>
         reaction = str(payload.emoji)
-        msg_id = payload.message_id
-        ch_id = payload.channel_id
-        user_id = payload.user_id
-        guild_id = payload.guild_id
-        print(reaction)
-        print("on_reaction_add called")
+        try:
+            rs_role = self.emojis[reaction]
+        except:
+            rs_role = -2
+        if(not payload.member.bot):
+            if(rs_role != -2):
+                if(rs_role != -1):
+                    await payload.member.add_roles(discord.utils.get(payload.member.guild.roles, name=f'RS{rs_role}'))
+                else:
+                    for role in payload.member.roles:
+                        if(str(role).startswith("RS")):
+                            await payload.member.remove_roles(role)
+                msg = await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
+                await msg.remove_reaction(reaction, payload.member)
+                
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload):
